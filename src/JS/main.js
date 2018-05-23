7189632a1a5b9364b7144252b3f0f1d7a3dbf7ca
jeffreyshen19 <jshen@andover.edu>
2018-05-23 16:02:14 -0400

    resize functionality

--- src/JS/main.js
+++ src/JS/main.js
@@ -2,20 +2,32 @@ drawCurve("AL");
 drawHistoricalCurve("historical-gk-AL");
 drawHistoricalCurve("historical-responsiveness-AL");
 drawHistoricalCurve("historical-symmetry-AL");
-drawDemonstrationCurve("step-1");
-drawDemonstrationCurve("step-2");
-drawDemonstrationCurve("step-3");
-drawDemonstrationCurve("step-4");
-drawDemonstrationCurve("not-symmetric");
-drawDemonstrationCurve("symmetric");
-drawDemonstrationCurve("proportional");
-drawDemonstrationCurve("incumbent-protection");
-drawDemonstrationCurve("winner-take-all");
-drawDemonstrationCurve("eg-optimized");
-drawDemonstrationCurve("partisan-bias");
-drawDemonstrationCurve("partisan-not-bias");
+drawFigures();
+
+function drawFigures(){
+  drawDemonstrationCurve("step-1");
+  drawDemonstrationCurve("step-2");
+  drawDemonstrationCurve("step-3");
+  drawDemonstrationCurve("step-4");
+  drawDemonstrationCurve("not-symmetric");
+  drawDemonstrationCurve("symmetric");
+  drawDemonstrationCurve("proportional");
+  drawDemonstrationCurve("incumbent-protection");
+  drawDemonstrationCurve("winner-take-all");
+  drawDemonstrationCurve("eg-optimized");
+  drawDemonstrationCurve("partisan-bias");
+  drawDemonstrationCurve("partisan-not-bias");
+}
 
 function redrawCurves(){
   drawCurve(d3.select(".active").attr('id'));
   drawHistoricalCurve(d3.select(".active-historical").attr('id'));
 }
+
+var resizeId;
+d3.select(window).on('resize', function(){
+  resizeId = setTimeout(function(){
+    drawFigures();
+    redrawCurves();
+  }, 500);
+});

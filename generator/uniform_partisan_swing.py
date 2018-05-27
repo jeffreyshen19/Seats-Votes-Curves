a34fed4c4a2d5a6de44b0bc5bf6cdd419537e752
jeffreyshen19 <jshen@andover.edu>
2018-05-27 19:02:31 -0400

    2012-2016 are comletely generated and good

--- generator/uniform_partisan_swing.py
+++ generator/uniform_partisan_swing.py
@@ -47,13 +47,36 @@ while i <= 1:
     totalDemSeats = 0
     for j in range(0,1000): #simulate 1000 elections
 
-        for district in votingByDistrict:
-            percentRepUpdated = district["percentRep"] + counter * SWING_CONST + SWING_CONST * random.randint(-5, 5)
-            percentDemUpdated = 1 - percentRepUpdated + diff
+        districtOverflowedRep = [False] * len(votingByDistrict);
+        districtOverflowedDem = [False] * len(votingByDistrict);
+        excessDem = 0
+        excessRep = 0
+        updatedValsRep = [0] * len(votingByDistrict);
+        updatedValsDem = [0] * len(votingByDistrict);
 
-            if percentRepUpdated > 0.50:
+        for k, district in enumerate(votingByDistrict):
+            updatedValsRep[k] = district["percentRep"] + counter * SWING_CONST + SWING_CONST * random.randint(-5, 5)
+            updatedValsDem[k] = 1 - updatedValsRep[k] + diff
+
+            if updatedValsRep[k] > 1:
+                excessRep += 1
+                districtOverflowedRep[k] = True
+
+            if updatedValsDem[k] < 0:
+                excessDem += 1
+                districtOverflowedDem[k] = True
+
+        for k, district in enumerate(votingByDistrict):
+            #Overflow mechanic: distribute excess votes to the other districts
+            if districtOverflowedRep[k] is False:
+                updatedValsRep[k] += SWING_CONST * (excessRep / (len(votingByDistrict) - excessRep))
+
+            if districtOverflowedDem[k] is False:
+                updatedValsDem[k] -= SWING_CONST * (excessDem / (len(votingByDistrict) - excessDem))
+
+            if updatedValsRep[k] > 0.50:
                 totalRepSeats += 1
-            if percentDemUpdated > 0.50:
+            if updatedValsDem[k] > 0.50:
                 totalDemSeats += 1
 
     i += SWING_CONST
@@ -71,13 +94,37 @@ while i <= 1:
     totalRepSeats = 0
 
     for j in range(0,1000): #simulate 1000 elections
-        for district in votingByDistrict:
-            percentDemUpdated = district["percentDem"] + counter * SWING_CONST + SWING_CONST * random.randint(-5, 5) + diff
-            percentRepUpdated = 1 - (percentDemUpdated - diff)
-            if percentDemUpdated > 0.50:
-                totalDemSeats += 1
-            if percentRepUpdated > 0.50:
+        districtOverflowedRep = [False] * len(votingByDistrict);
+        districtOverflowedDem = [False] * len(votingByDistrict);
+        excessDem = 0
+        excessRep = 0
+        updatedValsRep = [0] * len(votingByDistrict);
+        updatedValsDem = [0] * len(votingByDistrict);
+
+        for k, district in enumerate(votingByDistrict):
+            updatedValsDem[k] = district["percentDem"] + counter * SWING_CONST + SWING_CONST * random.randint(-5, 5) + diff
+            updatedValsRep[k] = 1 - (updatedValsDem[k] - diff)
+
+            if updatedValsRep[k] > 1:
+                excessRep += 1
+                districtOverflowedRep[k] = True
+
+            if updatedValsDem[k] < 0:
+                excessDem += 1
+                districtOverflowedDem[k] = True
+
+        for k, district in enumerate(votingByDistrict):
+            #Overflow mechanic: distribute excess votes to the other districts
+            if districtOverflowedRep[k] is False:
+                updatedValsRep[k] -= SWING_CONST * (excessRep / (len(votingByDistrict) - excessRep))
+
+            if districtOverflowedDem[k] is False:
+                updatedValsDem[k] += SWING_CONST * (excessDem / (len(votingByDistrict) - excessDem))
+
+            if updatedValsRep[k] > 0.50:
                 totalRepSeats += 1
+            if updatedValsDem[k] > 0.50:
+                totalDemSeats += 1
 
     i += SWING_CONST
     counter += 1
@@ -92,6 +139,7 @@ seatsVotesDem.insert(0, {"seats": 0, "votes": 0})
 seatsVotesRep.append({"seats": 1, "votes": 1})
 seatsVotesDem.append({"seats": 1, "votes": 1})
 
+# Write out to file
 outputFile.write("votes,seatsR,seatsD\n")
 for i in range(0, len(seatsVotesDem)):
     outputFile.write(str(100 * seatsVotesRep[i]["votes"]) + "," + str(100 * seatsVotesRep[i]["seats"]) + "," + str(100 * seatsVotesDem[i]["seats"]) + "\n")

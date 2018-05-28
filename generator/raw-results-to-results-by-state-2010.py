9e5d7e26145444366dcb70b04b3864e3bfc44440
jeffreyshen19 <jshen@andover.edu>
2018-05-27 21:17:31 -0400

    added 2002-2010

--- generator/raw-results-to-results-by-state-2010.py
+++ generator/raw-results-to-results-by-state-2010.py
@@ -41,17 +41,40 @@ with open(sys.argv[1], "rU") as csvfile:
                 stateNum += 1
                 currentAbbrev = states[stateNum]["abbrev"]
                 districtNum = district
+                rep = 0
+                dem = 0
 
             if district != districtNum: # Increment district
                 districtNum += 1
                 states[stateNum]["districts"].append(str(rep) + "," + str(dem))
+                rep = 0
+                dem = 0
 
-            if (party == "REP" or party == "R") and votes.isdigit():
-                rep = votes
-            elif party == "DEM" or party == "D" and votes.isdigit():
-                dem = votes
+            if votes == "Unopposed" or votes == "Unopposed   " or votes == "#":
+                votes = -1
+
+            if votes == "n/a" or votes == "??":
+                votes = 0
+
+            if party == "R" or party == "REP":
+                rep += int(votes)
+            elif party == "D" or party == "DFL" or party == "DEM":
+                dem += int(votes)
 
 states[49]["districts"].append(str(rep) + "," + str(dem))
+
+# Set uncontested districts as 75% and 25%
+for state in states:
+    for i in range(0, len(state["districts"])):
+        votingResults = state["districts"][i].split(",")
+        if int(votingResults[0]) == -1 and int(votingResults[1]) == 0:
+            state["districts"][i] = "75,25"
+        elif int(votingResults[0]) == 0 and int(votingResults[1]) == -1:
+            state["districts"][i] = "25,75"
+        elif int(votingResults[0]) == 0:
+            state["districts"][i] = str(int(votingResults[1]) / 3) + "," + votingResults[1]
+        elif int(votingResults[1]) == 0:
+            state["districts"][i] = votingResults[0] + "," + str(int(votingResults[0]) / 3)
 
 # Push to individual folder
 for state in states:
